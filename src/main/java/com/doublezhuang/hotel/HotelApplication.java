0babdd23457779b04a6566a952941ace1db321bb
Aye10032 <yeyu6_505@126.com>
2021-04-23 13:50:36 +0800

    实例化数据库初始化

--- src/main/java/com/doublezhuang/hotel/HotelApplication.java
+++ src/main/java/com/doublezhuang/hotel/HotelApplication.java
@@ -1,5 +1,6 @@
 package com.doublezhuang.hotel;
 
+import com.doublezhuang.hotel.database.DataInit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HotelApplication {
 
     public static void main(String[] args) {
+        DataInit.init();
         SpringApplication.run(HotelApplication.class, args);
     }
 
