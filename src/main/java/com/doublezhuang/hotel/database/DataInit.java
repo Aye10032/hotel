0bdc0e4639f1480bb77f37a3c0fff7b0bb07fc83
Aye10032 <yeyu6_505@126.com>
2021-04-23 11:55:52 +0800

    初始化数据库工具类

---
+++ src/main/java/com/doublezhuang/hotel/database/DataInit.java
@@ -0,0 +1,28 @@
+package com.doublezhuang.hotel.database;
+
+import com.doublezhuang.hotel.database.dao.DaoImpl;
+
+import java.io.File;
+
+/**
+ * @program: hotel
+ * @className: DataInti
+ * @Description: init database
+ * @version: v1.0
+ * @author: Aye10032
+ * @date: 2021/4/23 上午 11:43
+ */
+public class DataInit {
+
+    public static void init(){
+        File file = new File("hotel.db");
+        if (!file.exists()){
+            DaoImpl dao = new DaoImpl();
+
+            dao.CreatMemberTable();
+        }else {
+            //TODO
+        }
+    }
+
+}
