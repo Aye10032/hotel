e72432b55971b67dbc0de9a7bd3f96fefa7aafb9
doublezhuang101 <doublezhuang101@hotmail.com>
2021-06-08 12:53:41 +0800

    controller 补全

--- src/main/java/com/aye10032/hotel/controller/NewSubscriptionController.java
+++ src/main/java/com/aye10032/hotel/controller/NewSubscriptionController.java
@@ -1,8 +1,11 @@
 package com.aye10032.hotel.controller;
 
+import com.aye10032.hotel.database.dao.MemberDaoImpl;
 import com.aye10032.hotel.database.pojo.Member;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpSession;
 import javax.websocket.Session;
@@ -18,8 +21,14 @@ import javax.websocket.Session;
 @Controller
 public class NewSubscriptionController {
     @RequestMapping("/newSubscription")
-    public String newSubscription(Member member, HttpSession session)
+    public String newSubscription(
+            @RequestParam("name") String linkman,
+            Model model, HttpSession session)
     {
+        MemberDaoImpl dao = new MemberDaoImpl();
+        Member member = dao.FindMember(session.getAttribute("LoginUser").toString()).get(0);
+        model.addAttribute("name", member.getName());
+
         return "newSubscription";
     }
 }
