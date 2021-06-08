30f3a5acc8be3fbf4349a0b0be2e04b26c604c78
Aye10032 <yeyu6_505@126.com>
2021-06-08 16:50:50 +0800

    测试类

--- src/test/java/com/aye10032/hotel/HotelApplicationTests.java
+++ src/test/java/com/aye10032/hotel/HotelApplicationTests.java
@@ -1,5 +1,6 @@
 package com.aye10032.hotel;
 
+import com.aye10032.hotel.database.DataInit;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,11 @@ class HotelApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void testData(){
+        DataInit.init();
     }
 
 }
