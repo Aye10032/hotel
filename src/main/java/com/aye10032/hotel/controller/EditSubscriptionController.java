97f2b8b4aaec0827f4d0f464bb1f10d8080b8855
Aye10032 <yeyu6_505@126.com>
2021-06-10 14:40:26 +0800

    跳转到编辑界面

--- src/main/java/com/aye10032/hotel/controller/EditSubscriptionController.java
+++ src/main/java/com/aye10032/hotel/controller/EditSubscriptionController.java
@@ -4,6 +4,8 @@ import com.aye10032.hotel.database.dao.MemberDaoImpl;
 import com.aye10032.hotel.database.pojo.Member;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpSession;
@@ -19,15 +21,12 @@ import javax.servlet.http.HttpSession;
 @Controller
 public class EditSubscriptionController {
 
-    @RequestMapping("/editSubscription")
-    public String editSubscription(Model model, HttpSession session){
-        System.out.println("hiiii");
-        MemberDaoImpl dao = new MemberDaoImpl();
-        Member member = dao.FindMember(session.getAttribute("LoginUser").toString()).get(0);
-        System.out.println("hi " + member.getUsername());
-        model.addAttribute("name", member.getName());
-        model.addAttribute("phone", member.getPhone());
-        model.addAttribute("email", member.getEmail());
+    @RequestMapping("/editSubscription/{id}")
+    public String editSubscription(
+            @PathVariable("id")Integer id
+            ,Model model, HttpSession session){
+        System.out.println(id);
+
         return "editSubscription";
     }
 
