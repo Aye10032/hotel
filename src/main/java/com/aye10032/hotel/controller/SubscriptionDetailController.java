39f102c8fb490d72c80905775ce1b2ace403eeda
Aye10032 <yeyu6_505@126.com>
2021-06-12 14:12:26 +0800

    修正格式

--- src/main/java/com/aye10032/hotel/controller/SubscriptionDetailController.java
+++ src/main/java/com/aye10032/hotel/controller/SubscriptionDetailController.java
@@ -1,7 +1,11 @@
 package com.aye10032.hotel.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpSession;
 
 /**
  * @program: hotel
@@ -13,6 +17,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @Controller
 public class SubscriptionDetailController {
+
+    @RequestMapping("/subscriptionDetail/{sno}")
+    public String subscriptionDetail(
+            @PathVariable("sno")Integer id
+            , Model model, HttpSession session){
+        session.setAttribute("subID",id);
+
+        return "redirect:/subscriptionDetail";
+    }
 
     @RequestMapping("/subscriptionDetail")
     public String subscriptionDetail(){
