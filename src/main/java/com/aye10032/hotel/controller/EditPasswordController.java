699303c1294b58a4e7cf60303b1548687f6c946a
Aye10032 <yeyu6_505@126.com>
2021-06-14 12:41:00 +0800

    修改密码

--- src/main/java/com/aye10032/hotel/controller/EditPasswordController.java
+++ src/main/java/com/aye10032/hotel/controller/EditPasswordController.java
@@ -5,6 +5,7 @@ import com.aye10032.hotel.database.pojo.Member;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpSession;
 
@@ -21,8 +22,31 @@ import javax.servlet.http.HttpSession;
 public class EditPasswordController {
 
     @RequestMapping("/editPassword")
-    public String editPassword(Model model, HttpSession session){
-        System.out.println("in");
+    public String editPWD(){
         return "editPassword";
+    }
+
+    @RequestMapping("/edit_password")
+    public String editPassword(
+            @RequestParam("password") String password,
+            @RequestParam("newPassword") String newPassword,
+            @RequestParam("reNewPassword") String reNewPassword,
+            Model model, HttpSession session) {
+        MemberDaoImpl dao = new MemberDaoImpl();
+        Member member = dao.FindMember(session.getAttribute("LoginUser").toString()).get(0);
+
+        if (password.equals(member.getPwd())){
+            if (newPassword.equals(reNewPassword)){
+                member.setPwd(newPassword);
+                dao.UpdateMember(member);
+            }else {
+                model.addAttribute("msg","新密码不一致！");
+                return "editPassword";
+            }
+        }else {
+            model.addAttribute("msg","密码不正确！");
+            return "editPassword";
+        }
+        return "redirect:/register-2.html";
     }
 }
