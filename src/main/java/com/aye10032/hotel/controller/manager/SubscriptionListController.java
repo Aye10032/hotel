dd0b7bce0dec65c4a79ca3bdb2c38629f56b70f9
Aye10032 <yeyu6_505@126.com>
2021-06-16 17:19:54 +0800

    传参测试

--- src/main/java/com/aye10032/hotel/controller/manager/SubscriptionListController.java
+++ src/main/java/com/aye10032/hotel/controller/manager/SubscriptionListController.java
@@ -1,7 +1,12 @@
 package com.aye10032.hotel.controller.manager;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpSession;
+import java.util.Arrays;
 
 /**
  * @program: hotel
@@ -15,8 +20,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class SubscriptionListController {
 
     @RequestMapping("/manager/subscriptionList")
-    public String SubscriptionList(){
-        return "/manager/subscriptionList";
+    public String SubscriptionList(
+            @RequestParam("rootType")String[] rootType,
+            @RequestParam("status")String[] status,
+            @RequestParam("startDate")String startDate,
+            @RequestParam("endDate")String endDate,
+            @RequestParam("no")String no,
+            @RequestParam("username")String username,
+            Model model, HttpSession session){
+        System.out.println(Arrays.toString(rootType));
+        System.out.println(Arrays.toString(status));
+        System.out.println(startDate);
+        return "redirect:/manager/subscriptionList";
     }
 
 }
